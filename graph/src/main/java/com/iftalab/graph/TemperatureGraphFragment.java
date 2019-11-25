5951fbb090d0e03b3f07860ddc87b6991318b91b
ifta <iftalab@gmail.com>
2019-11-25 08:17:26 +0600

    start

---
+++ graph/src/main/java/com/iftalab/graph/TemperatureGraphFragment.java
@@ -0,0 +1,33 @@
+package com.iftalab.graph;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TemperatureGraphFragment extends Fragment {
+
+
+    public TemperatureGraphFragment() {
+        // Required empty public constructor
+    }
+
+    public static TemperatureGraphFragment newInstance(String param1, String param2) {
+        return new TemperatureGraphFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_temparature_graph, container, false);
+    }
+}
